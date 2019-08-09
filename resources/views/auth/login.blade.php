1d5fad2d660e24cd2082f70541977f41d86931bb
tomazneto <tomaz.aneto@gmail.com>
2019-08-08 21:48:38 -0300

    ajuste algumas mensagens de validação bem como seu idioma, e ajustes de layouts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
                                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                             <label class="form-check-label" for="remember">
-                                                {{ __('Lembrar depois') }}
+                                                {{ __('Lembrar-me') }}
                                             </label>
                                         </div>
                                     </div>
--- resources/views/layouts/_includes/topo.blade.php
+++ resources/views/layouts/_includes/topo.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Verum') }}</title>
+    <title>Verum</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -41,11 +41,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Entrar') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('LOGIN') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('REGISTRAR') }}</a>
                                 </li>
                             @endif
                         @else
